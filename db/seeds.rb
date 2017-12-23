1768d58bb44a576e22177805a3a086bbe2f0a58d
Fatima Burke <fatima@lurmarketing.com>
2017-12-22 19:58:01 -0500

    created seeded user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#create dummy users
+user = User.create! :username => 'fatimaburke',
+                    :email => 'test@test.com',
+                    :password => '123456',
+                    :password_confirmation => '123456',
+                    :bio => 'I\'m into all kinds of great and cool music. Check me out!',
+                    :genre => 'Gospel',
+                    :location => 'Philadelphia, PA',
+                    :instrument => 'Piano',
+                    :website => 'www.fatimaburke.com'
+
+user2 = User.create! :username => 'geneburke',
+                    :email => 'test2@test.com',
+                    :password => '123456',
+                    :password_confirmation => '123456',
+                    :bio => 'I\'m a pretty darn good piano player',
+                    :genre => 'Jazz',
+                    :location => 'Philadelphia, PA',
+                    :instrument => 'Keyboards',
+                    :website => 'www.facebook.com/genekeyska'
