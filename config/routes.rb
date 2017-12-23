a9928d32b4d9a02b5fe03886ecf5992b075878e2
Fatima Burke <fatima@lurmarketing.com>
2017-12-22 19:56:41 -0500

    restricting resources for stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
 
-  resources :streams
+  resources :streams, only: [:new, :create, :show, :index, :edit]
+  resources :users
+  resources :messages
 
   root 'home#index'
+
+  mount ActionCable.server => '/cable'
+
+  # get 'streams/:id' => 'streams#show'
+  post 'streams/:id' => 'streams#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
