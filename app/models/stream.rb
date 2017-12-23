030a23d0fc18b96c866490ac470310558522a94b
Fatima Burke <fatima@lurmarketing.com>
2017-12-22 19:52:56 -0500

    added message relationship to stream

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,4 +1,11 @@
 class Stream < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true,
                     length: { minimum: 3 }
+
+  has_many :messages, dependent: :destroy
+
+  has_attached_file :thumbnail, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
 end
