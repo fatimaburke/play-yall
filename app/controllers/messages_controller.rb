2319d15730c201eecd618728de381a875d5e4407
Fatima Burke <fatima@lurmarketing.com>
2017-12-22 19:59:28 -0500

    added message model

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,14 @@
+class MessagesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.save!
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
