b73f0c1ff3d1fc19abe5420ef997a75fb979f8fd
Fatima Burke <fatima@lurmarketing.com>
2017-12-22 19:59:11 -0500

    Added Chats controller

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,10 @@
+class ChatsController < ApplicationController
+  # def chat
+  #   opentok = OpenTok::OpenTok.new '46027242', '3f2d5e712234b65a45a0d9c34839e9f59aa870d1'
+  #   @session = opentok.create_session
+  #   # @session_id = @session.session_id
+  #   @session_id = @session.session_id
+  #   # generate opentok token
+  #   @token = @session_id.generate_token
+  # end
+end
